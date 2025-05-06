7dca2f7aa6fe3819eb3bf1254eb78e279b566a40
Anubhav <anubhav.kumar1@icloud.com>
2025-05-06 22:52:35 +0530

    Removed old folders: brainwave-main, Documents_project, server

--- PCS25-10/brainwave-main/src/components/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const PrivateRoute = ({ children }) => {
-  const token = sessionStorage.getItem("token"); // ✅ Check if user is authenticated
-  return token ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
