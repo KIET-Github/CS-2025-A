3795015ffbc5f088bbb6774d2c704e296c204605
Ishita Srivastava <114524123+ishita0302@users.noreply.github.com>
2025-05-24 14:14:00 +0530

    Delete PCS25-09/src directory

--- PCS25-09/src/utils/auth.js
+++
@@ -1,9 +0,0 @@
-export const getAuthHeader = () => {
-    const token = localStorage.getItem('token');
-    
-    if (token) {
-      return { Authorization: `Bearer ${token}` };
-    }
-    
-    return {};
-  };
